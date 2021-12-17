9ee188dcf8f1de41e5e75137b0a03c2de128eb51
sushantdesaur <desaursushant@gmail.com>
2021-12-17 12:41:51 +0530

    made stripe template

--- resources/views/stripe.blade.php
+++ resources/views/stripe.blade.php
@@ -1,129 +1,159 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Laravel 8 - Stripe Payment Gateway </title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" />
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <style type="text/css">
-        .panel-title {
-        display: inline;
-        font-weight: bold;
-        }
-        .display-table {
-            display: table;
-        }
-        .display-tr {
-            display: table-row;
-        }
-        .display-td {
-            display: table-cell;
-            vertical-align: middle;
-            width: 61%;
-        }
-    </style>
-</head>
-<body>  
+@extends('welcome')
+
+@section('title', 'Stripe Payment Gateway')
+
+@section('navbar')
+@endsection
+
+@section('content')
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
 <div class="container">
-  
-    <h1>Laravel 8 - Stripe Payment Gateway</h1>
-  
     <div class="row">
-        <div class="col-md-6 col-md-offset-3">
-            <div class="panel panel-default credit-card-box">
-                <div class="panel-heading display-table" >
-                    <div class="row display-tr" >
-                        <h3 class="panel-title display-td" >Payment Details</h3>
-                        <div class="display-td" >                            
-                            <img class="img-responsive pull-right" src="http://i76.imgup.net/accepted_c22e0.png">
-                        </div>
-                    </div>                    
+        <div class="col">
+            <div class="card">
+                <div class="card-header bg-primary">
+                    <h3 class="card-title text-white m-0" >Payment Details</h3>
                 </div>
-                <div class="panel-body">
-  
+                <div class="card-body" >
                     @if (Session::has('success'))
-                        <div class="alert alert-success text-center">
-                            <a href="#" class="close" data-dismiss="alert" aria-label="close">×</a>
-                            <p>{{ Session::get('success') }}</p>
-                        </div>
+                    <div class="alert alert-success text-center">
+                        <a href="#" class="close" data-dismiss="alert" aria-label="close">×</a>
+                        <p>{{ Session::get('success') }}</p>
+                    </div>
                     @endif
-  
                     <form 
-                            role="form" 
-                            action="{{ route('stripe.post') }}" 
-                            method="post" 
-                            class="require-validation"
-                            data-cc-on-file="false"
-                            data-stripe-publishable-key="{{ env('STRIPE_KEY') }}"
-                            id="payment-form">
+                        role="form" 
+                        action="{{ route('stripe.post') }}" 
+                        method="post" 
+                        class="require-validation"
+                        data-cc-on-file="false"
+                        data-stripe-publishable-key="{{ env('STRIPE_KEY') }}"
+                        id="payment-form">
                         @csrf
-                        <!-- <div class='form-row row'>
-                            <div class='col-xs-12 form-group required'>
-                                <label class='control-label'>Customer Name</label> <input
-                                    class='form-control' size='4' type='text'>
-                            </div>
-                        </div>
-                        <div class='form-row row'>
-                            <div class='col-xs-12 form-group required'>
-                                <label class='control-label'>Address</label> <input
-                                    class='form-control' size='4' type='text'>
-                            </div>
-                        </div> -->
-                        <div class='form-row row'>
-                            <div class='col-xs-12 form-group required'>
-                                <label class='control-label'>Name on Card</label> <input
-                                    class='form-control' size='4' type='text'>
-                            </div>
-                        </div>
-  
-                        <div class='form-row row'>
-                            <div class='col-xs-12 form-group card required'>
-                                <label class='control-label'>Card Number</label> <input
-                                    autocomplete='off' class='form-control card-number' size='20'
-                                    type='text'>
-                            </div>
-                        </div>
-  
-                        <div class='form-row row'>
-                            <div class='col-xs-12 col-md-4 form-group cvc required'>
-                                <label class='control-label'>CVC</label> <input autocomplete='off'
-                                    class='form-control card-cvc' placeholder='ex. 311' size='4'
-                                    type='text'>
-                            </div>
-                            <div class='col-xs-12 col-md-4 form-group expiration required'>
-                                <label class='control-label'>Expiration Month</label> <input
-                                    class='form-control card-expiry-month' placeholder='MM' size='2'
-                                    type='text'>
-                            </div>
-                            <div class='col-xs-12 col-md-4 form-group expiration required'>
-                                <label class='control-label'>Expiration Year</label> <input
-                                    class='form-control card-expiry-year' placeholder='YYYY' size='4'
-                                    type='text'>
-                            </div>
-                        </div>
-  
-                        <div class='form-row row'>
-                            <div class='col-md-12 error form-group hide'>
-                                <div class='alert-danger alert'>Please correct the errors and try
-                                    again.</div>
-                            </div>
-                        </div>
-  
                         <div class="row">
-                            <div class="col-xs-12">
+                            {{-- Billing Info --}}
+                            <div class="col-xl-6 col-md-12">
+                                <div class="row">
+                                    <div class="col-xl-6 col-md-12">
+                                        <div class="form-floating mb-3 required">
+                                            <input type="text" class='form-control' id="floatingInput" placeholder="First Name"> 
+                                            <label for="floatingInput">First Name</label>             
+                                        </div>
+                                    </div>
+                                    <div class="col-xl-6 col-md-12">
+                                        <div class="form-floating mb-3 required">
+                                            <input type="text" class='form-control' id="floatingInput" placeholder="Last Name"> 
+                                            <label for="floatingInput">Last Name</label>             
+                                        </div>                                
+                                    </div>
+                                    <div class="col-xl-12 col-md-12">
+                                        <div class="form-floating mb-3">
+                                            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                                            <label for="floatingInput">Email address</label>
+                                        </div>                                
+                                    </div>
+                                    <div class="col-xl-12 col-md-12">
+                                        <div class="form-floating mb-3">
+                                            <input type="text" class="form-control" id="floatingInput" placeholder="Address Line 1">
+                                            <label for="floatingInput">Adress Line 1</label>
+                                        </div>                                
+                                    </div>
+                                    <div class="col-xl-12 col-md-12">
+                                        <div class="form-floating mb-3">
+                                            <input type="text" class="form-control" id="floatingInput" placeholder="Address Line 2">
+                                            <label for="floatingInput">Adress Line 2</label>
+                                        </div>                                
+                                    </div>
+                                    <div class="col-xl-6 col-md-12">
+                                        <div class="form-floating mb-3">
+                                            <input type="text" class="form-control" id="floatingInput" placeholder="City or Village">
+                                            <label for="floatingInput">City or Village</label>
+                                        </div>                                
+                                    </div>
+                                    <div class="col-xl-6 col-md-12">
+                                        <div class="form-floating mb-3">
+                                            <input type="text" class="form-control" id="floatingInput" placeholder="State">
+                                            <label for="floatingInput">State</label>
+                                        </div>                                
+                                    </div>
+                                    <div class="col-xl-6 col-md-12">
+                                        <div class="form-floating mb-3">
+                                            <input type="text" class="form-control" id="floatingInput" placeholder="Zip Code">
+                                            <label for="floatingInput">Zip Code</label>
+                                        </div>                                
+                                    </div>
+                                    <div class="col-xl-6 col-md-12">
+                                        <div class="form-floating mb-3">
+                                            <input type="text" class="form-control" id="floatingInput" placeholder="Contact Number">
+                                            <label for="floatingInput">Contact Number</label>
+                                        </div>                                
+                                    </div>
+                                </div>
+                            </div>
+
+
+                            {{-- Card Column --}}
+                            <div class="col-xl-6 col-md-12">
+                                <div class="row">
+                                    <div class="col-xl-12 col-md-12">
+                                        <div class='form-floating card mb-3 required'>
+                                            <input autocomplete='off' class='form-control' id="floatingInput" placeholder="Course Name">
+                                            <label for="floatingInput">Course Name</label> 
+                                        </div>
+                                    </div>
+                                    <div class="col-xl-12 col-md-12">
+                                        <div class='form-floating card mb-3 required'>
+                                            <input autocomplete='off' class='form-control' id="floatingInput" placeholder="Amount">
+                                            <label for="floatingInput">Amount</label> 
+                                        </div>
+                                    </div>
+                                    <div class="col-xl-12 col-md-12">
+                                        <div class='form-floating card mb-3 required'>
+                                            <input autocomplete='off' class='form-control card-number' id="floatingInput" placeholder="Enter card number">
+                                            <label for="floatingInput">Card Number</label> 
+                                        </div>
+                                    </div>
+                                    <div class="col-xl-4 col-md-12 ">
+                                        <div class='form-floating cvc mb-3 required'>
+                                            <input autocomplete='off' class='form-control card-cvc' id="floatingInput" placeholder="ex. 311">
+                                            <label for="floatingInput">CVC</label>    
+                                        </div>
+                                    </div>
+                                    <div class="col-xl-4 col-md-12 ">
+                                        <div class='form-floating expiration mb-3 required'>
+                                            <input class='form-control card-expiry-month' id="floatingInput" placeholder='MM'>
+                                            <label for="floatingInput">Expiration Month</label>
+                                        </div>
+                                    </div>
+                                    <div class="col-xl-4 col-md-12">
+                                        <div class='form-floating expiration mb-3 required'>
+                                            <input class='form-control card-expiry-year' id="floatingInput" placeholder='YYYY'>
+                                            <label for="floatingInput">Expiration Year</label> 
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            
+                            
+                            <div class="col-xl-6 col-md-12">
                                 <button class="btn btn-primary btn-lg btn-block" type="submit">Pay Now ($100)</button>
                             </div>
                         </div>
-                          
-                    </form>
+                        
+                            {{-- <div class='form-row row'>
+                                <div class='col-md-12 error form-floating hide'>
+                                    <div class='alert-danger alert'>Please correct the errors and try
+                                        again.</div>
+                                </div>
+                            </div> --}}            
+                    </form>                 
                 </div>
             </div>        
         </div>
     </div>
-      
 </div>
-  
-</body>
-  
+    
 <script type="text/javascript" src="https://js.stripe.com/v2/"></script>
   
 <script type="text/javascript">
@@ -182,4 +212,9 @@ $(function() {
    
 });
 </script>
-</html>
+
+@endsection
+
+
+
+
